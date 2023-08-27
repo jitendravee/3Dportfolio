c13904af2e3ad47121053cabcf0d1b6ad5c78bfd
jitendravee <jitendravee098@gmail.com>
2023-08-27 09:12:16 +0530

    third

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles'
+import { ComputerCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+    <div {`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-rowitems-start gap-5`}>
+      <div className="flex flex-col justify-center items-center mt-5">
+        <div className="w-5 h-5 rounded-full bg[#916eff]" />
+      </div>
+
+    </div>
+  </section>
   )
 }
 
 export default Hero
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -43,7 +43,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='jlogo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Jitendra &nbsp;
             <span className='sm:block hidden'> | Choudhary</span>
